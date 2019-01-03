986ba36fda32e1d4ae2e751f6fe991602cdca8a9
bing.zhub <bing.zhub@gmail.com>
2019-01-03 14:03:30 +0800

    finish cart remove

--- eQin/app/src/main/java/com/example/bing/eqin/controller/CartController.java
+++ eQin/app/src/main/java/com/example/bing/eqin/controller/CartController.java
@@ -1,16 +1,20 @@
 package com.example.bing.eqin.controller;
 
 import android.content.Context;
+import android.util.Log;
 
 import com.example.bing.eqin.model.CartItem;
 import com.example.bing.eqin.model.StoreItem;
 import com.example.bing.eqin.utils.CommonUtils;
 import com.parse.DeleteCallback;
+import com.parse.FindCallback;
 import com.parse.ParseException;
 import com.parse.ParseObject;
 import com.parse.ParseQuery;
 import com.parse.ParseUser;
 import com.parse.SaveCallback;
+
+import java.util.List;
 
 public class CartController {
     private static  CartController mInstance;
@@ -49,16 +53,36 @@ public class CartController {
     }
 
     public void removeFromCart(CartItem cartItem, final Context context){
-        ParseObject object = new ParseObject("UserCart");
-        object.put("User", ParseUser.getCurrentUser());
-        object.put("Product", cartItem.getProduct().getItemName());
-        object.deleteInBackground(new DeleteCallback() {
+        ParseQuery<ParseObject> query = ParseQuery.getQuery("UserCart");
+        query.whereEqualTo("User", ParseUser.getCurrentUser());
+        query.whereEqualTo("Product", cartItem.getProduct().getItemName());
+        Log.d("removeCart", ""+cartItem);
+        query.findInBackground(new FindCallback<ParseObject>() {
             @Override
-            public void done(ParseException e) {
-                if(e == null){
-                    CommonUtils.showMessage(context, "已删除");
+            public void done(List<ParseObject> objects, ParseException e) {
+                if(e==null && objects.size() !=0){
+                    ParseObject object = objects.get(0);
+                    object.deleteInBackground(new DeleteCallback() {
+                        @Override
+                        public void done(ParseException e) {
+                            if(e==null){
+                                CommonUtils.showMessage(context, "删除成功");
+                            }
+                        }
+                    });
                 }
             }
         });
+//        ParseObject object = new ParseObject("UserCart");
+//        object.put("User", ParseUser.getCurrentUser());
+//        object.put("Product", cartItem.getProduct().getItemName());
+//        object.deleteInBackground(new DeleteCallback() {
+//            @Override
+//            public void done(ParseException e) {
+//                if(e == null){
+//                    CommonUtils.showMessage(context, "已删除");
+//                }
+//            }
+//        });
     }
 }
